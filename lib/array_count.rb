98d199141286243256eaaff006435d84abec7fdf
Rene Avila <reneavila1993@gmail.com>
2020-05-07 23:36:34 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,13 +1,13 @@
-def count_strings(array)
-  array.count do |n|
-    n.class == String
-  end
-end 
+# def count_strings(array)
+#   array.count do |n|
+#     n.class == String
+#   end
+# end 
 
     
     
-def count_empty_strings(array)
-  array.count do |n|
-    n.class == String && n.size == 0
-  end
-end
+# def count_empty_strings(array)
+#   array.count do |n|
+#     n.class == String && n.size == 0
+#   end
+# end
